0c170578009ef605ab679da12fc816dbbfe6bba2
ZhenFengWu <1994583917@qq.com>
2023-09-13 11:55:12 +0800

    feat:shell script

--- linux/install/sh-install-pg.sh
+++ linux/install/sh-install-pg.sh
@@ -14,7 +14,7 @@ if [ -n "$1" ]; then
         echo "用途：安装配置postgresql数据库，使用的是官网下载的源码安装包"
         echo "参数：参数                    是否必传    说明"
         echo "      --help; -help; -h        false      查看脚本使用说明"
-        echo "      -p                       true      安装>=安装postgresql14，指定安装包路径"
+        echo "      -p                       true       安装>=安装postgresql14，指定安装包路径"
         echo "************************************************************************"
         exit 0
     else
@@ -124,56 +124,68 @@ function pg() {
     #   -U 选择数据库superuser的用户名。默认 为运行initdb的用户的名称。而postgresql数据库的默认名称是postgres，所以创建用户组和用户的时候名字是postgres
     #   -W 对于新的超级用户提示输入口令
     #   -E 指定数据库编码，一般为UTF8。这也是稍后创建任何数据库的默认编码
+    echo "------> 初始化"
     su - postgres -c "${pg_bin}/initdb -E UTF8 -D ${pg_home}/data"
     # 启动服务
+    echo "------> 启动服务"
     su - postgres -c "${pg_bin}/pg_ctl -D ${pg_home}/data start"
-    # 修改数据库访问密码
-    su - postgres -c "${pg_bin}/psql -c alter user postgres with encrypted password '123456';"
-    # 重启服务（自行选择是否重启）
+    # 修改数据库访问密码（结尾 EOF" 前面不能有空格，单独一行，不要格式化缩进，否则报错）
+    echo "------> 修改数据库访问密码"
+    su - postgres -c "${pg_bin}/psql <<EOF
+    \set ON_ERROR_STOP on
+    alter user postgres with encrypted password '123456';
+    \q
+EOF"
+    # 重启服务，或停止服务（自行选择是否重启）
+    echo "------> 重启服务，或停止服务"
     #su - postgres -c "${pg_bin}/pg_ctl -D ${pg_home}/data restart"
+    su - postgres -c "${pg_bin}/pg_ctl -D ${pg_home}/data stop"
     # 安装好的pg主目录权限，交给用户postgres
     chown -R postgres:postgres "${pg_home}"
 
-
-
+    echo ""
     echo "安装成功，数据库目录为：${pg_home}"
     echo "数据库密码为：123456"
     echo "==================================================== 根据需求，添加额外配置 ===================================================="
-    echo "远程访问（postgres权限操作）：vim配置文件postgresql.conf"
-    echo "              修改参数 listen_address = ’*’ 监听所有访问"
-    echo "              修改参数 password_encryption = scram-sha-256  密码验证开启"
-    echo "         vim配置文件pg_hba.conf"
-    echo "              ip范围配置（所有客户端都可以访问），在大约90行左右哪里，添加一行：host    all        all        0.0.0.0/0         scram-sha-256"
+    echo "远程访问（postgres权限操作）：vim配置文件：${pg_home}/data/postgresql.conf"
+    echo "              修改参数 listen_address = ’*’ 监听所有访问（大约在60行附近）"
+    echo "              修改参数 password_encryption = scram-sha-256  密码验证开启（大约在96行附近）"
+    echo "         vim配置文件：${pg_home}/data/pg_hba.conf"
+    echo "              ip范围配置（所有客户端都可以访问），在大约90行附近，添加一行：host    all             all             0.0.0.0/0               scram-sha-256"
     echo ""
-    echo "开机自启（root权限操作）：vim添加配置文件postgresql.service：vim /usr/lib/systemd/system/postgresql.service"
+    echo "开机自启（root权限操作）：vim添加配置文件postgresql.service：sudo vim /usr/lib/systemd/system/postgresql.service"
     echo "              添加如下内容："
-    echo "                  [Unit]"
-    echo "                  Description=PostgreSQL database server"
-    echo "                  After=network.target"
-    echo "                  [Service]"
-    echo "                  Type=forking"
-    echo "                  User=postgres"
-    echo "                  Group=postgres"
-    echo "                  # Port number for server to listen on"
-    echo "                  Environment=PG_PORT=5432"
-    echo "                  # Location of database directory"
-    echo "                  Environment=PG_DATA=${pg_home}/data/"
-    echo "                  # Where to send early-startup messages from the server (before the logging"
-    echo "                  # options of postgresql.conf take effect)"
-    echo "                  # This is normally controlled by the global default set by systemd"
-    echo "                  # StandardOutput=syslog"
-    echo "                  # Disable OOM kill on the postmaster"
-    echo "                  OOMScoreAdjust=-1000"
-    echo "                  #ExecStartPre=${pg_home}/bin/postgresql-check-db-dir \${PG_DATA}"
-    echo "                  ExecStart=${pg_home}/bin/pg_ctl start -D \${PG_DATA} -s -o \"-p \${PG_PORT}\" -w -t 300"
-    echo "                  ExecStop=${pg_home}/bin/pg_ctl stop -D \${PG_DATA} -s -m fast"
-    echo "                  ExecReload=${pg_home}/bin/pg_ctl reload -D \${PG_DATA} -s"
-    echo "                  # Give a reasonable amount of time for the server to start up/shut down"
-    echo "                  TimeoutSec=300"
-    echo "                  [Install]"
-    echo "                  WantedBy=multi-user.target"
-    echo ""
+    echo "                    [Unit]"
+    echo "                    Description=PostgreSQL database server"
+    echo "                    After=network.target"
+    echo "                    [Service]"
+    echo "                    Type=forking"
+    echo "                    User=postgres"
+    echo "                    Group=postgres"
+    echo "                    # Port number for server to listen on"
+    echo "                    Environment=PG_PORT=5432"
+    echo "                    # Location of database directory"
+    echo "                    Environment=PG_DATA=${pg_home}/data/"
+    echo "                    # Where to send early-startup messages from the server (before the logging"
+    echo "                    # options of postgresql.conf take effect)"
+    echo "                    # This is normally controlled by the global default set by systemd"
+    echo "                    # StandardOutput=syslog"
+    echo "                    # Disable OOM kill on the postmaster"
+    echo "                    OOMScoreAdjust=-1000"
+    echo "                    #ExecStartPre=${pg_home}/bin/postgresql-check-db-dir \${PG_DATA}"
+    echo "                    ExecStart=${pg_home}/bin/pg_ctl start -D \${PG_DATA} -s -o \"-p \${PG_PORT}\" -w -t 300"
+    echo "                    ExecStop=${pg_home}/bin/pg_ctl stop -D \${PG_DATA} -s -m fast"
+    echo "                    ExecReload=${pg_home}/bin/pg_ctl reload -D \${PG_DATA} -s"
+    echo "                    # Give a reasonable amount of time for the server to start up/shut down"
+    echo "                    TimeoutSec=300"
+    echo "                    [Install]"
+    echo "                    WantedBy=multi-user.target"
     echo "              设置权限：chmod 777 /usr/lib/systemd/system/postgresql.service"
+    echo "              设置开机自启：systemctl enable postgresql.service"
+    echo "              启动服务：systemctl start postgresql.service"
+    echo "              停止服务：systemctl stop postgresql.service"
+    echo "              重启服务：systemctl reload postgresql.service"
+    echo "              服务状态：systemctl status postgresql.service"
 
 }
 
